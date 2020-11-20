686a3bf72563abc5576bc578b4e770d203caaad6
1053226965@qq.com <1053226965@qq.com>
2020-11-20 19:02:44 +0800

    1

--- include/common/coroutine/strand_task.hpp
+++ include/common/coroutine/strand_task.hpp
@@ -12,13 +12,13 @@ namespace mrpc::detail
       bool await_ready() noexcept;
       std::experimental::coroutine_handle<> await_suspend(std::experimental::coroutine_handle<> handle) noexcept;
       void await_resume() noexcept;
-      strand_task_t & _strand_task;
+      strand_task_t &_strand_task;
     };
 
     struct node_t
     {
       std::experimental::coroutine_handle<> _coroutine;
-      node_t* _next { nullptr };
+      node_t *_next{nullptr};
     };
     friend class awaitable_t;
 
@@ -27,7 +27,7 @@ namespace mrpc::detail
     auto operator co_await() noexcept { return awaitable_t{*this}; }
 
   private:
-    std::atomic<node_t*> _head;
+    std::atomic<node_t *> _head;
   };
 
   bool strand_task_t::awaitable_t::await_ready() noexcept
@@ -37,9 +37,15 @@ namespace mrpc::detail
 
   std::experimental::coroutine_handle<> strand_task_t::awaitable_t::await_suspend(std::experimental::coroutine_handle<> coroutine) noexcept
   {
-    node_t *node = new node_t{ coroutine };
-    node_t *t_node = _strand_task._head.exchange(node, std::memory_order_acq_rel);
-    if(t_node == nullptr)
+    node_t *node = new node_t{coroutine};
+    node_t *t_node = _strand_task._head.load(std::memory_order_acquire);
+    node->_next = t_node;
+    while (!_strand_task._head.compare_exchange_weak(t_node, node))
+    {
+      t_node = _strand_task._head.load(std::memory_order_acquire);
+      node->_next = t_node;
+    }
+    if (t_node == nullptr)
     {
       return coroutine;
     }
@@ -52,11 +58,9 @@ namespace mrpc::detail
 
   void strand_task_t::awaitable_t::await_resume() noexcept
   {
-    
   }
 
   strand_task_t::strand_task_t() noexcept
   {
-
   }
 } // namespace mrpc::detail
